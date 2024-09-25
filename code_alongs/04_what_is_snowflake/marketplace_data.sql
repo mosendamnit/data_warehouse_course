ad81525e2b3a0423d66712a19fca0885b0ee5906
Muhsin Ahmad <mohsin_afridi99@hotmail.com>
2024-09-25 19:36:48 +0200

    sql repeatiton in snowflake

--- code_alongs/04_what_is_snowflake/marketplace_data.sql
+++ code_alongs/04_what_is_snowflake/marketplace_data.sql
@@ -12,6 +12,7 @@ SHOW VIEWS;
 
 SELECT * FROM organization_summary LIMIT 10;
 
+SELECT * FROM organization_summary WHERE COUNTRY_CODE = 'PAK' LIMIT 10;
 
 DESC VIEW organization_summary;
 
@@ -31,4 +32,17 @@ SELECT COUNT(DISTINCT country_code) FROM organization_summary;
 SELECT country_code , COUNT(*) AS number_organizations
 FROM organization_summary
 GROUP BY country_code
-ORDER BY number_organization DESC;
+ORDER BY number_organizations DESC;
+
+
+SELECT country_code , COUNT(*) AS number_organizations
+FROM organization_summary
+GROUP BY country_code
+HAVING country_code IN ('SWE' , 'DNK' , 'NOR' , 'PAK' , 'FIN')
+ORDER BY number_organizations DESC;
+
+SELECT country_code , COUNT(*) AS number_organizations , region
+FROM organization_summary
+GROUP BY country_code , region
+HAVING country_code IN ('SWE' , 'DNK' , 'NOR' , 'PAK' , 'FIN')
+ORDER BY number_organizations DESC;
